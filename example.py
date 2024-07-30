355584836d8e48fe51d0b71372df0fe3b6763896
Ryan McClanahan <rpm@slac.stanford.edu>
2024-07-30 09:51:43 -0700

    added header to example

--- example.py
+++ example.py
@@ -1,1 +1,6 @@
-# TODO: make an example
+import ScPatternSelect
+
+# --- WARNING ---
+# This exapmle contains code that when ran will change the rate on prod
+# The code that will change the rate
+# To run the example and develop with this code use lcls-dev3

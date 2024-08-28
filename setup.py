bb3aae5470b13cc28d4be7d95d2b8a3d4f58ffc3
Ryan McClanahan <rpm@slac.stanford.edu>
2024-08-28 16:26:48 -0700

    incremented version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ScPatternSelection',
-    version='0.0.1',
+    version='0.1.1',
     description='A module for selecting the sc pattern',
     long_description=long_description,
 
